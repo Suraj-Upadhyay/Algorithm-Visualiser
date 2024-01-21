7f96ce46d35c70bfeb16985bd6f8d30b62136b01
Suraj Upadhyay <usuraj35@gmail.com>
2024-01-21 13:37:39 +0530

    Add the constants file
    This constants file would define the limits
    of our controls and the list of algorithms
    that are supported or are not supported.

---
+++ src/constants.js
@@ -0,0 +1,37 @@
+export const MINDATABARS = 5;
+export const MAXDATABARS = 100;
+export const DEFAULTDATABARS = MINDATABARS;
+export const MINTIME = 10;
+export const MAXTIME = 60;
+export const DEFAULTTIME = MINTIME;
+export const MINDATASPREAD = 50;
+export const MAXDATASPREAD = 1000;
+export const DEFAULTDATASPREAD = MINDATASPREAD;
+
+export const ALGO_IMPLEMENTATION_LIST = {
+    'Selection Sort': null,
+    'Bubble Sort': null,
+    'Insertion Sort': null,
+    'Merge Sort': null,
+    'Quick Sort': null,
+    'Heap Sort': null,
+    'Counting Sort': null,
+    'Radix Sort': null,
+    'Bucket Sort': null,
+    'Bingo Sort': null,
+    'Shell Sort': null,
+    'Tim Sort': null,
+    'Comb Sort': null,
+    'Pigeonhole Sort': null,
+    'Cycle Sort': null,
+    'Cocktail Sort': null,
+    'Strand Sort': null,
+    'Bitonic Sort': null,
+    'Pancake Sort': null,
+    'Permutation Sort': null,
+    'Gnome Sort': null,
+    'Sleep Sort': null,
+    'Stooge Sort': null,
+    'Tree Sort': null,
+    'Brick Sort': null
+};
