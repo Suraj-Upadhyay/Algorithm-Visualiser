a359febc2eeca69fa8c273bc1f538b3a93d32b08
Suraj Upadhyay <usuraj35@gmail.com>
2024-03-25 03:54:21 +0530

    Refactor: Delete files that reappeared after rebase

--- src/components/visualiser/ActionItem.tsx
+++ src/components/visualiser/ActionItem.tsx
@@ -5,7 +5,7 @@ interface IActionItemParams {
   onPlayControlAction: (event: React.MouseEvent<HTMLDivElement>) => void;
 }
 
-function ActionItem(props: IActionItemParams) {
+function ActionItem(props: IActionItemParams): JSX.Element {
   return (
     <div className="float-left w-[25%] h-full box-border bg-[rgba(255,_255,_255,_0.1)] transition-colors duration-1000 hover:bg-[rgba(255,_255,_255,_0.5)] first:rounded-tl-[10px] first:rounded-bl-[10px] last:rounded-tr-[10px] last:rounded-br-[10px]">
       <div className="flex flex-col h-full w-full cursor-pointer" onClick={props.onPlayControlAction}>
