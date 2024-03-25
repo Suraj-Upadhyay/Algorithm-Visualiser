8940416c6f6208344d9a5119bf16a5d943c4bc0a
Suraj Upadhyay <usuraj35@gmail.com>
2024-03-25 09:45:50 +0530

    Add: Visualiser: Implement step forward and backward
    Fix: Disabling behavior inside playcontrol

--- src/components/visualiser/ActionItem.tsx
+++ src/components/visualiser/ActionItem.tsx
@@ -1,19 +1,28 @@
 import React from "react";
+import { type IconType } from "react-icons";
 
 interface IActionItemParams {
-  googleIcon: string;
-  onPlayControlAction: (event: React.MouseEvent<HTMLDivElement>) => void;
+  disabled: boolean;
+  Icon: IconType;
+  onPlayControlAction: () => void;
 }
 
 function ActionItem(props: IActionItemParams): JSX.Element {
+
+  const handleClick = (event: React.MouseEvent<HTMLDivElement>):void => {
+    event.preventDefault();
+    if (!props.disabled)
+      props.onPlayControlAction();
+  }
+
   return (
     <div className="float-left box-border h-full w-[25%] bg-[rgba(255,_255,_255,_0.1)] transition-colors duration-1000 first:rounded-bl-[10px] first:rounded-tl-[10px] last:rounded-br-[10px] last:rounded-tr-[10px] hover:bg-[rgba(255,_255,_255,_0.5)]">
       <div
-        className="flex h-full w-full cursor-pointer flex-col"
-        onClick={props.onPlayControlAction}
+        className={`flex h-full w-full cursor-pointer flex-col ${props.disabled ? "opacity-50": ""}`}
+        onClick={handleClick}
       >
-        <span className="material-symbols-outlined m-auto block w-6">
-          {props.googleIcon}
+        <span className="m-auto block w-6">
+          <props.Icon size={24} color="white" />
         </span>
       </div>
     </div>
